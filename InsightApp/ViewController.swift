069cb1fe809a3656a47998e7d2667b4f0ec6662a
Kesley Ribeiro <kesley.developer@icloud.com>
2017-05-15 10:51:07 -0300

    Initial Commit

---
+++ InsightApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InsightApp
+//
+//  Created by Kesley Ribeiro on 15/May/17.
+//  Copyright © 2017 Kesley Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
